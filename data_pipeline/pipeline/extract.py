a32b180e33ed4363a5ba45b026ff9ce4d9560b00
Roberto Park <betopark97@gmail.com>
2025-05-25 19:00:41 +0900

    Initialize data_pipeline scripts

---
+++ data_pipeline/pipeline/extract.py
@@ -0,0 +1,9 @@
+from utils.steam_api_manager import SteamAPIManager
+from utils.mongo_manager import MongoManager
+
+
+# --------------------------------------------------------------------------
+# Managers for data extraction
+# --------------------------------------------------------------------------
+steam_api_manager = SteamAPIManager()
+mongo_manager = MongoManager()
